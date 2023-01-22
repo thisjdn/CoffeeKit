d21c9a218b9c26e730e017d261027ae6e7e7be68
Jaden Hong <jaden@Jadens-MacBook-Air.local>
2023-01-21 21:53:45 -0500

    MapKit used, map added, pin added

---
+++ CoffeeKit/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  CoffeeKit
+//
+//  Created by Jaden Hong on 2023-01-21.
+//
+
+import Foundation
+import CoreLocation
+
+
+struct Constants {
+    
+    static let testLongitude : CLLocationDegrees = 139.6503
+    static let testLatitude : CLLocationDegrees = 35.6762
+    
+    
+}
--- CoffeeKit/ViewController.swift
+++ CoffeeKit/ViewController.swift
@@ -5,16 +5,50 @@
 //  Created by Jaden Hong on 2023-01-21.
 //
 
-import UIKit 
+import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    let testLocation = CLLocationCoordinate2D(latitude: Constants.testLatitude, longitude: Constants.testLongitude)
+    
+    let mapView : MKMapView = {
+        let map = MKMapView()
+        return map
+    }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setMap()
+         
+        setPlacemark(location: testLocation)
+         
     }
-     
+    
+    func setMap() {
+        view.addSubview(mapView)
+        
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        mapView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        mapView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+    }
 
+    
+    func setPlacemark(location: CLLocationCoordinate2D) {
+        var pin = MKPlacemark(coordinate: location)
+        let coordinateRegion = MKCoordinateRegion(center: pin.coordinate, latitudinalMeters: 800, longitudinalMeters: 800)
+        
+        let name = pin.description.description
+        
+        mapView.setRegion(coordinateRegion, animated: true)
+        mapView.addAnnotation(pin) 
+    }
+    
 
 }
 
